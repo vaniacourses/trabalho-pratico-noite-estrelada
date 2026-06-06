2406a0a5db8bb877a6909e313e813fc142f74b04
EMV271828 <emv2.71828@gmail.com>
2026-06-06 18:50:51 -0300

    criação de interface DTO para Livro e Midia

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,20 +1,33 @@
+export interface IMidiaDTO {
+    id?: string;
+    tipo: string;
+    titulo: string;
+    dataCriacao: Date
+}
+
+export interface ILivroDTO extends IMidiaDTO{
+    autor: string;
+    isbn: string;
+    paginas: number;
+}
+
 export interface IRealizarEmprestimoDTO {
-  idLeitor: string;
-  idExemplar: string;
-  diasEmprestimo?: number;
+    idLeitor: string;
+    idExemplar: string;
+    diasEmprestimo?: number;
 }
 
 export interface IEmprestimoResponse {
-  id: string;
-  idLeitor: string;
-  idExemplar: string;
-  dataInicio: Date;
-  dataExpiracao: Date;
-  estado: string;
+    id: string;
+    idLeitor: string;
+    idExemplar: string;
+    dataInicio: Date;
+    dataExpiracao: Date;
+    estado: string;
 }
 
 export interface IErroAplicacao {
-  codigo: string;
-  mensagem: string;
-  statusHttp: number;
+    codigo: string;
+    mensagem: string;
+    statusHttp: number;
 }
