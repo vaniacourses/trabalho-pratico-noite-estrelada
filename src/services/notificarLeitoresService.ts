9ca2c64cacf7043ae7533658046de8b3cb4d4786
EMV271828 <emv2.71828@gmail.com>
2026-06-14 11:07:50 -0300

    modificações no metodo enviarNotificação

--- src/services/notificarLeitoresService.ts
+++ src/services/notificarLeitoresService.ts
@@ -1,7 +1,7 @@
 import {EmprestimoRepository} from "@/repositories/emprestimoRepository.ts";
 import {ReservaRepository} from "@/repositories/reservaRespository.ts";
 import {LeitorRepository} from "@/repositories/leitorRepository.ts";
-import {Exemplar, Reserva} from "@prisma/client";
+import {EstadoLeitor, EstadoReserva, Exemplar, Reserva} from "@prisma/client";
 import {prisma} from "@/lib/prisma.ts";
 
 export class NotificarLeitoresService {
@@ -9,9 +9,20 @@ export class NotificarLeitoresService {
     private emprestimoRepository: EmprestimoRepository;
     private leitorRespository: LeitorRepository
 
-    private tratamentoDasReservas(reservas: Reserva []): Reserva[] {
 
-        reservas.sort((a, b) =>
+    private enviarNotificacao(idLeitor: string, index: number): void {
+        console.log(`O leitor ${idLeitor} está na posição ${index} da lista de espera para este exemplar.`);
+    }
+
+    private async tratamentoDasReservas(exemplarDisponivel: Exemplar): Promise<Reserva[] | null> {
+
+        const reservas = await
+            this.reservaRespository.obterReservasPorMidiaId(exemplarDisponivel.idMidia);
+
+        const reservasFiltradas = reservas.filter(reserva =>
+            reserva.leitor.estado === "REGULAR" as EstadoLeitor);
+
+        reservasFiltradas.sort((a, b) =>
             a.dataCriacao.getTime() - b.dataCriacao.getTime()
         );
 
@@ -19,25 +30,33 @@ export class NotificarLeitoresService {
     }
 
     async notificarLeitores(exemplarDisponivel: Exemplar): Promise<any> {
-        const reservas = await
-            this.reservaRespository.obterReservasPorMidiaId(exemplarDisponivel.idMidia);
 
-        if (!reservas) {
+        const reservasTratadas = await this.tratamentoDasReservas(exemplarDisponivel);
+
+        if (!reservasTratadas) {
             console.log("Não há reservas para este exemplar")
             return {status: `Não há reservas para o exemplar ${exemplarDisponivel.id}`};
-        }
 
-        const reservasTratadas = this.tratamentoDasReservas(reservas);
+        }
 
         // a reserva vai mudar seu estado e sera criado um emprestimo para ela. o leitor sera notificado
         await prisma.$transaction(async (tx) => {
+            const primeiraReserva = reservasTratadas[0];
+
+            await tx.reserva.update({
+                where: {id: primeiraReserva.id},
+                data: {estado: "BLOQUEANTE" as EstadoReserva},
+            });
+
 
         });
 
         // as demais reservas serão apenas para notificar os leitores de sua posição na "lista de espera"
-        for (const reserva of reservasTratadas.slice(1, reservas.length - 1)) {
 
-        }
+        reservasTratadas.slice(1, reservasTratadas.length - 1)
+            .forEach((reserva, index) => {
+                this.enviarNotificacao(reserva.idLeitor, index);
+            });
 
     }
 
